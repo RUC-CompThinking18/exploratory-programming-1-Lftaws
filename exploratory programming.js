5b32f2c9542980e52d0c97533929cd06a367463f
Logan <loganf96@gmail.com>
2018-10-04 15:39:08 -0400

    Added comments

--- exploratory programming.js
+++ exploratory programming.js
@@ -13,17 +13,22 @@
   alert (List);
   var List2 = Funk (List);
   alert (List2);
+
   //Function made to hold  the block of code for the for loop.
   function Funk(List, Mult)
   {
   //variable used to hold the value of doubling the array of numbers on variable List
     var Mult = [];
+
     //The loop to count up the array in List until it reaches the last number.
     for (var I=0; I < List.length ; I++)
     {
+
       //I holds the value of the given number in the array, and multiplied by 5, then continues to count up in the array.
-      Mult.push(List[I] * 5);
+      // Mult is the variable called in the function Funk, and push allows Mult to hold the value of each number in list
+      Mult.push(List[I] * 5;
       alert (I+1);
+
     }
     // Displays the numbers from the array multiplied by 5
     return Mult;
